5bcac2a8462e3dc6f5f78f9c0d4e17e0e1d793fd
Nicolas Chourrout <nicolasc@blinkbox.com>
2013-12-11 08:51:41 +0000

    Initial Commit

---
+++ FestivalPrototype/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FestivalPrototype
+//
+//  Created by Nicolas Chourrout on 11/12/2013.
+//  Copyright (c) 2013 Nicolas Chourrout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FestivalPrototypeTests/FestivalPrototypeTests.m
@@ -0,0 +1,34 @@
+//
+//  FestivalPrototypeTests.m
+//  FestivalPrototypeTests
+//
+//  Created by Nicolas Chourrout on 11/12/2013.
+//  Copyright (c) 2013 Nicolas Chourrout. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FestivalPrototypeTests : XCTestCase
+
+@end
+
+@implementation FestivalPrototypeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
