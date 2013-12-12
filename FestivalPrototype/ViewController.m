85498a251e171ae419e7cd82fbabd4a21a41382c
Nicolas Chourrout <nicolasc@blinkbox.com>
2013-12-12 16:53:22 +0000

    Initial commit

--- FestivalPrototype/ViewController.m
+++ FestivalPrototype/ViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "AEAudioController.h"
+#import "AEAudioFilePlayer.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) AEAudioController *audioController;
+@property (nonatomic, strong) NSArray *trackFiles;
+@property (nonatomic, strong) NSArray *filesForPlayer;
 @end
 
 @implementation ViewController
@@ -18,6 +22,48 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self loadTracks];
+    [self configurePlayer];
+}
+
+
+- (void)loadTracks
+{
+    _trackFiles = @[
+                    @{@"filename" : @"1.mp3", @"type" : @"mp3"},
+                    @{@"filename" : @"2.mp3", @"type" : @"mp3"}
+                    ];
+}
+
+- (void)configurePlayer
+{
+    self.audioController = [[AEAudioController alloc]
+                           initWithAudioDescription:[AEAudioController interleaved16BitStereoAudioDescription]
+                           inputEnabled:NO];
+    self.audioController.preferredBufferDuration = 0.093;
+    self.audioController.allowMixingWithOtherApps = NO;
+    
+    NSMutableArray *tempFilesForPlayer = [[NSMutableArray alloc] init];
+    
+    // LOOP THROUGH TO SET UP EACH AEAUDIOUNITFILEPLAYER
+    for(NSDictionary *track in _trackFiles) {
+        AEAudioUnitFilePlayer *fileForPlayer = [AEAudioUnitFilePlayer audioUnitFilePlayerWithController:_audioController error:nil];
+        fileForPlayer.volume = 0.75;
+        fileForPlayer.currentTime = _currentTrackTimePassed; // DEFAULTS TO '0'
+        [tempFilesForPlayer addObject:fileForPlayer];
+    }
+    _filesForPlayer = [[NSArray alloc] initWithArray:tempFilesForPlayer];
+    
+    _group = [_audioController createChannelGroup];
+    [_audioController addChannels:_filesForPlayer toChannelGroup:_group];
+    [_audioController setVolume:0.75 forChannelGroup:_group];
+    
+    // LOOP TO SET EACH AEAUDIOUNITFILEPLAYER URL
+    [_trackFiles enumerateObjectsUsingBlock:^(id track, NSUInteger idx, BOOL *stop) {
+        NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:track[@"filename"] ofType:track[@"type"] inDirectory:_directory]];
+        [_filesForPlayer[idx] setUrl:url];
+    }];
+    _totalTrackTime = [_filesForPlayer[0] duration]; // THEY'RE ALL THE SAME LENGTH SO I JUST GET THE FIRST ONE
 }
 
 - (void)didReceiveMemoryWarning
---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios
+pod 'TheAmazingAudioEngine', '~> 1.2'
