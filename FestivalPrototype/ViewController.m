2828480d295503f4056690e7b6d8b50fc3c51369
Michal Pietras <pietrasm@me.com>
2013-12-18 14:24:17 +0000

    Cleaned track label when music is not playing.

--- FestivalPrototype/ViewController.m
+++ FestivalPrototype/ViewController.m
@@ -414,6 +414,8 @@
     if (title && artist && ![title isEqualToString:@""] && ![artist isEqualToString:@""]) {
 //        user.trackLabel.text = [NSString stringWithFormat:@"%@ by %@", title, artist];
         user.trackLabel.text = artist;
+    } else {
+        user.trackLabel.text = nil;
     }
 
     dispatch_async(dispatch_get_global_queue(0,0), ^{
